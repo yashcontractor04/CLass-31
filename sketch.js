cc73122f164d9ce36d50503133b38b94d68fbc32
yashcontractor04 <yashcontractor04@gmail.com>
2020-08-01 19:47:14 +0530

    Trajectory

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
 
+var gameState= "onSling";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -69,16 +70,19 @@ function draw(){
 }
 
 function mouseDragged(){
+    if(gameState !== "launch"){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
 }
 
 
 function mouseReleased(){
+    gameState="launch";
     slingshot.fly();
 }
 
-function keyPressed(){
+/*function keyPressed(){
     if(keyCode === 32){
         slingshot.attach(bird.body);
     }
-}
+}*/
